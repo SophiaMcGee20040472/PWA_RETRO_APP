c4409ba1d7ace35264fa3bb4ee2d6acc4d377334
SophiaMcGee20040472 <83893260+SophiaMcGee20040472@users.noreply.github.com>
2023-03-31 12:29:20 +0100

    alter import for frontend deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
